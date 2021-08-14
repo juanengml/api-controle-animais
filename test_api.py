6cfc13065a6a59150db89cc039a80532e288f27c
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-08-14 16:59:13 +0000

    Deon !

--- test_api.py
+++ test_api.py
@@ -1,21 +1,37 @@
 from requests import get,post,put,delete
 import requests 
 
-endpoint = "http://44.196.172.92:8080/"
+endpoint = "http://firmadataflavor.ddns.net:8080/"
 
 status = get(endpoint).json()
 
 print(status)
 
 endpoint_crud = "{}api/v1/crud/".format(endpoint)
-
-data = {'nome':"lora",
+data = [{'nome':"lora",
 		    'dt_nascimento':"05/02/2019",
 		    'especie':"gato",
 		    'insta':"@lora"
+},
+{'nome':"ada",
+		    'dt_nascimento':"01/11/2019",
+		    'especie':"gato",
+		    'insta':"@lora"
+},
+{'nome':"sam",
+		    'dt_nascimento':"01/11/2019",
+		    'especie':"gato",
+		    'insta':"@lora"
+},
+{'nome':"uly",
+		    'dt_nascimento':"01/11/2019",
+		    'especie':"gato",
+		    'insta':"@lora"
 }
-r = post(endpoint_crud,data).json()
-print("POST: ",r)
+]
+for value in data:
+  r = post(endpoint_crud,value).json() 
+  print("POST: ",r)
 
 data = {"nome":"lora"}
 r = get(endpoint_crud,data).json()
